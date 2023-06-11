02fd5a93d1a528e577853c96cae2e67727cea118
miguelbcr <miguel@irewardhealth.com>
2023-06-11 10:50:40 +0200

    Integrate pigeon sample `BookApi` on Android

--- android/app/src/main/kotlin/com/example/flutter_showcase/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_showcase/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.example.flutter_showcase
 
+import androidx.annotation.NonNull
+import dev.flutter.pigeon.Book
+import dev.flutter.pigeon.BookApi
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+private class PigeonApiImplementation : BookApi {
+    override fun search(keyword: String): List<Book> {
+        val result = Book(
+            title = String.format("%s's Book", keyword),
+            author = keyword
+        )
+        return listOf(result)
+    }
+}
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        val api = PigeonApiImplementation()
+        BookApi.setUp(flutterEngine.dartExecutor.binaryMessenger, api);
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_showcase/pigeon.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,67 +49,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController _controller = TextEditingController(text: "");
+  final BookApi _api = BookApi();
+  final List<Book?> _apiResult = List.empty(growable: true);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Result:',
             ),
             Text(
-              '$_counter',
+              _apiResult.map((e) => "${e!.title} - ${e.author}").join(", "),
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            TextField(
+              maxLength: 15,
+              controller: _controller,
+              textInputAction: TextInputAction.done,
+              onSubmitted: (text) {
+                _api.search(text).then((value) {
+                  debugPrint("searching for $text, result=$value");
+                  setState(() {
+                    _apiResult.clear();
+                    _apiResult.addAll(value);
+                  });
+                }, onError: (error, stacktrace) {
+                  debugPrint(error);
+                  debugPrintStack(stackTrace: stacktrace);
+                });
+              },
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
